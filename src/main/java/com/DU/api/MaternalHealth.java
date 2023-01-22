733b80f4de18d4221266bbbc931b4f025d7b2fa8
Kwizera Claude <71407602+kclaudeeager@users.noreply.github.com>
2023-01-22 18:04:05 +0200

    Just going to test

--- src/main/java/com/DU/api/MaternalHealth.java
+++ src/main/java/com/DU/api/MaternalHealth.java
@@ -8,6 +8,9 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -51,8 +54,8 @@ public class WebConfiguration implements WebMvcConfigurer {
             .addMapping("/**")
             .allowedMethods("*")
             .allowedHeaders("*")
-            .allowedOrigins("*")
-            .allowCredentials(true);
+                .allowedOrigins("*")
+                .allowCredentials(false);
     }
 
 }
